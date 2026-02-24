592845ff4c786b4e5976b29e0d178018792e348d
LoydNgei <loydngei@gmail.com>
2026-02-24 21:50:20 +0300

    Created User, Wallet and Transaction migrations and models

---
+++ app/Models/Wallet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
