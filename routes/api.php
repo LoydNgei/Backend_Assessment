f0f1f559a8e2b3159fb988992817635fd9104a1a
LoydNgei <loydngei@gmail.com>
2026-02-25 13:41:23 +0300

    Defined API Routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WalletController;
+use App\Http\Controllers\Api\TransactionController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    // Users
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+
+    // Wallets
+    Route::post('/wallets', [WalletController::class, 'store']);
+    Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
+
+    // Transactions
+    Route::post('/transactions', [TransactionController::class, 'store']);
+});
